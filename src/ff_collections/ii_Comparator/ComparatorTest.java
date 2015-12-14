b3321407b507018a60990b895e7b781ab3479f8d
Kai Nagel <2008@kainagel.org>
2015-12-14 21:53:43 +0100

    maintenance

--- src/ff_collections/ii_Comparator/ComparatorTest.java
+++ src/ff_collections/ii_Comparator/ComparatorTest.java
@@ -2,15 +2,12 @@ package ff_collections.ii_Comparator;
 
 import java.util.*;
 
-import ff_collections.hh_Comparable.Agent;
-
-
 class ComparatorTest
 {
 	public static void main ( String[] args ) {
 		List<Agent> agents = new ArrayList<Agent>() ;
 		
-//		System.out.println ( "\ngenerate agents with random id/age ..." ) ;
+		System.out.println ( "\ngenerate agents with random id/age ..." ) ;
 		for ( int ii=0 ; ii<5 ; ii++ ) {
 			double age = Math.random();
 			int id = (int)(100000.*Math.random());
@@ -18,22 +15,20 @@ class ComparatorTest
 			agents.add(ag) ;
 		}
 		
-//		System.out.println ( "\ndemonstrate that id/age are in rnd sequence: " ) ;
-//		for ( Agent ag : agents ) {
-//			System.out.println ( " agentId: " + ag.getId() + " age: " + ag.getAge() ) ;
-//		}
+		System.out.println ( "\ndemonstrate that id/age are in rnd sequence: " ) ;
+		printAllAgents(agents);
 
-//		System.out.println ( "\nsort the agents ..." ) ;
-//		Collections.sort( agents ) ;
+		System.out.println ( "\nsort the agents according to CMP ..." ) ;
+		Collections.sort( agents /* , insert anonymous class here */ ) ;
 
-//		System.out.println ( "\nsort the agents according to CMP ..." ) ;
-		IdComparator c = new IdComparator();
-		Collections.sort( agents, c ) ;
-//
-//		System.out.println ( "\nprint out the agents: " ) ;
+		System.out.println ( "\nprint out the agents: " ) ;
+		printAllAgents(agents) ;
+
+	}
+
+	private static void printAllAgents(List<Agent> agents) {
 		for ( Agent ag : agents ) {
-			System.out.println ( " id: " + ag.getId() + " age: " + ag.getAge() ) ;
+			System.out.println ( " agentId: " + ag.getId() + " age: " + ag.getAge() ) ;
 		}
-
 	}
 }
