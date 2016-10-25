557e3041d4c4c462c81c62a38219209e55a51aa2
kainagel <nagel@vsp.tu-berlin.de>
2016-10-25 11:47:38 +0200

    newest version

--- src/nn_junit/HelloWorld.java
+++ src/nn_junit/HelloWorld.java
@@ -2,20 +2,20 @@ package nn_junit;
 
 public class HelloWorld {
 
-	private boolean sayHello = false;
+	private static boolean sayHello = false;
 	
-	public String getHello(){
-		return "Hello World";
+	static String getHello() {
+		return "Hello World" ;
 	}
 	
-	public String sayHelloGoodbye(){
+	static String sayHelloGoodbye(){
 		if (sayHello){
-			this.sayHello = false;
-			return "and I say hello...";
+			sayHello = false;
+			return "and I say hello ...";
 		}
 		else {
-			this.sayHello = true;
-			return "You say goodbye...";
+			sayHello = true;
+			return "You say goodbye ...";
 		}
 	}
 	
@@ -23,10 +23,9 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println(new HelloWorld().getHello());
-		HelloWorld hw = new HelloWorld();
-		System.out.println(hw.sayHelloGoodbye());
-		System.out.println(hw.sayHelloGoodbye());
+		System.out.println(getHello());
+		System.out.println(sayHelloGoodbye());
+		System.out.println(sayHelloGoodbye());
 	}
 
 }
--- src/nn_junit/HelloWorldTest.java
+++ src/nn_junit/HelloWorldTest.java
@@ -7,36 +7,32 @@ import org.junit.Test;
 
 public class HelloWorldTest {
 
-	private HelloWorld fixture;
-	private static final String hello = "and I say hello...";
-	private static final String goodbye = "You say goodbye...";
+	private static final String hello = "and I say hello ...";
+	private static final String goodbye = "You say goodbye ...";
 	
 	@Before
 	public void setUp(){
-		this.fixture = new HelloWorld();
 	}
 	
 	
 	@Test
 	public void testHelloWorld() {
-		Assert.assertEquals("Hello World", this.fixture.getHello());
-	}
-
-	@Test
-	public void testGoodbye() {
-		Assert.assertEquals("You don't say goodbye?", goodbye, this.fixture.sayHelloGoodbye());
+		Assert.assertEquals("Hello World", HelloWorld.getHello());
 	}
 
 	@Test
 	public void testGoodbyeHello(){
-		for (int i = 0; i <= 10; i++){
-			if (i % 2 == 0){
-				Assert.assertEquals(goodbye, this.fixture.sayHelloGoodbye());
-			}
-			else {
-				Assert.assertEquals(hello, this.fixture.sayHelloGoodbye());
-			}
-		}
+		Assert.assertEquals("You don't say goodbye?", goodbye, HelloWorld.sayHelloGoodbye());
+
+//		for (int i = 0; i <= 10; i++){
+//			System.out.println("i=" + i );
+//			if (i % 2 == 0){
+//				Assert.assertEquals(hello, HelloWorld.sayHelloGoodbye());
+//			}
+//			else {
+//				Assert.assertEquals(goodbye, HelloWorld.sayHelloGoodbye());
+//			}
+//		}
 	}
 	
 }
