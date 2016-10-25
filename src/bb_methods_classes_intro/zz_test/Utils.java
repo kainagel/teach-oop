69f4c61df288696717e54e555a10412f96fe5b67
kainagel <nagel@vsp.tu-berlin.de>
2016-10-25 14:47:02 +0200

    a line near top of utils

--- src/bb_methods_classes_intro/zz_test/Utils.java
+++ src/bb_methods_classes_intro/zz_test/Utils.java
@@ -2,6 +2,8 @@ package bb_methods_classes_intro.zz_test;
 
 public class Utils {
 
+	// another line
+	
 	static double doSomethingWith( double aa, double bb ) {
 		System.out.println( "using the double/double method");
 		return aa*bb ;
