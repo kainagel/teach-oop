30a5335d3199079f242695ed9045a5300b13c6ea
kainagel <nagel@vsp.tu-berlin.de>
2018-10-15 21:24:46 +0200

    already staged changes from 2 years ago

--- src/aa_basics/bb_helloWorld/HelloWorld.java
+++ src/aa_basics/bb_helloWorld/HelloWorld.java
@@ -3,18 +3,7 @@ package aa_basics.bb_helloWorld;
 class HelloWorld {
 	
 	public static void main  ( String[] args ) {
-		System.out.println ( "Hello World" + " and there it continues") ;
-		
-		int a = 5 ;
-
-		int b = 7 ;
-		int c = a + b ;
-		
-		for ( int i=0 ; i<20 ; i++ ) {
-		
-			System.out.println( c + "  "+  c + " i=" + i) ;
-			
-		}
-		 
+		System.out.println ( "Hello World" ) ;
 	}
+
 }
