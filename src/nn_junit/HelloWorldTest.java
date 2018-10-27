6823654c2fba658cd879bb7dcc920be4c9bd08e3
kainagel <nagel@vsp.tu-berlin.de>
2018-10-27 11:08:34 +0200

    comment out material for class preparation

--- src/nn_junit/HelloWorldTest.java
+++ src/nn_junit/HelloWorldTest.java
@@ -10,34 +10,38 @@ public class HelloWorldTest {
 	private static final String hello = "and I say hello ...";
 	private static final String goodbye = "You say goodbye ...";
 	
-	@Before
-	public void setUp(){
-	}
-	
-	
-	@Test
-	public void testHelloWorld() {
-		Assert.assertEquals("Hello World", HelloWorld.getHello());
-	}
+//	@Before
+//	public void setUp(){
+//	}
 	
 	@Test
-	public void testGoodbye() {
-		Assert.assertEquals("You don't say goodbye?", goodbye, HelloWorld.sayHelloGoodbye());
+	public void testFail() {
+		Assert.fail();
 	}
-
-	@Test
-	public void testGoodbyeHello(){
-		Assert.assertEquals("You don't say goodbye?", goodbye, HelloWorld.sayHelloGoodbye());
-
-//		for (int i = 0; i <= 10; i++){
-//			System.out.println("i=" + i );
-//			if (i % 2 == 0){
-//				Assert.assertEquals(hello, HelloWorld.sayHelloGoodbye());
-//			}
-//			else {
-//				Assert.assertEquals(goodbye, HelloWorld.sayHelloGoodbye());
-//			}
-//		}
-	}
+	
+//	@Test
+//	public void testHelloWorld() {
+//		Assert.assertEquals("Hello World", HelloWorld.getHello());
+//	}
+//	
+//	@Test
+//	public void testGoodbye() {
+//		Assert.assertEquals("You don't say goodbye?", goodbye, HelloWorld.sayHelloGoodbye());
+//	}
+//
+//	@Test
+//	public void testGoodbyeHello(){
+//		Assert.assertEquals("You don't say goodbye?", goodbye, HelloWorld.sayHelloGoodbye());
+//
+////		for (int i = 0; i <= 10; i++){
+////			System.out.println("i=" + i );
+////			if (i % 2 == 0){
+////				Assert.assertEquals(hello, HelloWorld.sayHelloGoodbye());
+////			}
+////			else {
+////				Assert.assertEquals(goodbye, HelloWorld.sayHelloGoodbye());
+////			}
+////		}
+//	}
 	
 }
