787b43f35b9d39b1138071321dd50981139a0f9c
kainagel <nagel@vsp.tu-berlin.de>
2019-11-05 14:40:12 +0100

    various changes in context of not forgetting source directory any more

--- src/main/java/nn_junit/Utils.java
+++ src/main/java/nn_junit/Utils.java
@@ -2,12 +2,19 @@ package nn_junit;
 
 public class Utils{
 
-	static double myMax( double aa, double bb ) {
-		if ( aa > bb ) {
-			return aa ;
-		} else {
-			return bb ;
+
+	static double myMax( double aa, double bb, double cc ) {
+		double result = Double.MIN_VALUE ;
+		if ( aa > result ) {
+			result = aa ;
 		}
+		if ( bb > result ) {
+			result = bb ;
+		}
+		if ( cc > result ) {
+			result = cc ;
+		}
+		return result ;
 	}
 	
 }
