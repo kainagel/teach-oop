e241bfaf2d75dfe4c2b9d466a90ae2ece4756bbe
kainagel <nagel@vsp.tu-berlin.de>
2020-11-05 13:08:30 +0100

    update

---
+++ src/main/java/ooIEEE/bbDecimalAndBinary/Abc.java
@@ -0,0 +1,16 @@
+package ooIEEE.bbDecimalAndBinary;
+
+import java.math.BigDecimal;
+
+class Abc{
+
+	public static void main( String[] args ){
+
+		System.out.println(  0.1 + 0.2 );
+
+//		BigDecimal aa = new BigDecimal( 0.1 );
+//		BigDecimal bb = new BigDecimal( 0.2 );
+//		System.out.println( aa.add( bb ) );
+
+	}
+}
