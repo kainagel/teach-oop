27e36aa8f646489f0e584e26a1e1e95b890b40a5
Kai Nagel <nagel@vsp.tu-berlin.de>
2022-10-27 15:21:06 +0200

    changes from oop'22

--- src/main/java/bb_methods_classes_intro/aa_Math/Main.java
+++ src/main/java/bb_methods_classes_intro/aa_Math/Main.java
@@ -1,11 +1,19 @@
 package bb_methods_classes_intro.aa_Math;
 
 
+import static java.lang.Math.*;
+
 class Main{
 
 	public static void main  ( String[] args ) {
 
-		System.out.println( Math.sin( 1.3 ) );
+		System.out.println( sin( PI/2. ) );
+
+		System.out.println( expm1( 0.001 ) );
+
+		System.out.println( ( exp(-0.1 ) ) / ( exp( -0.1 ) + exp( -0.3 )) ) ;
+
+		System.out.println( sin(3.0) ) ;
 
 	}
 
