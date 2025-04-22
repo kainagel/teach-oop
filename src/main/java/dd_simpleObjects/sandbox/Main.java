6f64e4201cd0b6576dcb12e9521966217dee7dae
Kai Nagel <nagel@vsp.tu-berlin.de>
2025-04-22 11:38:20 +0200

    commit new file

---
+++ src/main/java/dd_simpleObjects/sandbox/Main.java
@@ -0,0 +1,18 @@
+package dd_simpleObjects.sandbox;
+
+class Main{
+
+	public static void main( String[] args ){
+
+
+		Double aDouble = 20.;
+
+		aDouble.doubleValue();
+
+		Double result = Double.valueOf( "20" );
+
+		double result2 = Double.parseDouble( "20" );
+	}
+
+
+}
